c314abad3bf96e15e8bc6cf1b041cce9babfac1f
yanming <325787573@qq.com>
2017-06-07 16:51:47 +0800

    页面修改

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,6 +1,7 @@
 /* 
 Google Maps
 */
+/*
 var map = '';
 var center;
 
@@ -25,13 +26,14 @@ function initialize() {
 function calculateCenter() {
   center = map.getCenter();
 }
+*/
 
-function loadGoogleMap(){
+/*function loadGoogleMap(){
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
     document.body.appendChild(script);
-}
+}*/
 
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
